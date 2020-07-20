45b801dd7b87c58c4bed1fffafae1ac9552c90e7
Andy Bulka <abulka@gmail.com>
2020-07-20 16:17:19 +1000

    back to main devel

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:webview_flutter/webview_flutter.dart';
+
+// Uses the regular webview, with an asset being loaded from the filesystem
+
+void main() => runApp(MyApp());
+
+var lastResult = "nothing yet";
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        "/": (_) => WebViewTest(),
+      },
+    );
+  }
+}
+
+class WebViewTest extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _WebViewTestState();
+  }
+}
+
+class _WebViewTestState extends State<WebViewTest> {
+  //
+  WebViewController _webViewController;
+  String filePath = 'assets/index_main.html';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Webview Little JS World')),
+      body: WebView(
+        initialUrl: '',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _webViewController = webViewController;
+          _loadHtmlFromAssets();
+        },
+        javascriptChannels: <JavascriptChannel>[
+          _toasterJavascriptChannel(context),
+        ].toSet(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text('Calling JS...')),
+          );
+          // _webViewController.evaluateJavascript('fred_add(10, 10)');
+          _webViewController
+              .evaluateJavascript('fred_add_via_timeout_which_posts(10, 10)');
+        },
+      ),
+    );
+  }
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message) {
+          print('message from javascript: ${message.message}');
+          lastResult = message
+              .message; // andy secret way to comm to other code - should perhaps update a provider model?
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        });
+  }
+
+  _loadHtmlFromAssets() async {
+    String fileHtmlContents = await rootBundle.loadString(filePath);
+    _webViewController.loadUrl(Uri.dataFromString(fileHtmlContents,
+            mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
+        .toString());
+  }
+}
