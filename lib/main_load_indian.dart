ab1b6a3069629ccbc074c88dfa4cba3049098036
Andy Bulka <abulka@gmail.com>
2020-07-20 10:54:21 +1000

    indian example works

---
+++ lib/main_load_indian.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:webview_flutter/webview_flutter.dart';
+
+// http://www.coderzheaven.com/2019/05/25/load-local-html-file-into-webview-in-flutter-call-js-function-from-flutter/
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        "/": (_) => WebViewTest(),
+      },
+    );
+  }
+}
+
+class WebViewTest extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _WebViewTestState();
+  }
+}
+
+class _WebViewTestState extends State<WebViewTest> {
+  //
+  WebViewController _webViewController;
+  String filePath = 'assets/indian.html';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Webview Demo')),
+      body: WebView(
+        initialUrl: '',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _webViewController = webViewController;
+          _loadHtmlFromAssets();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          _webViewController.evaluateJavascript('add(10, 10)');
+        },
+      ),
+    );
+  }
+
+  _loadHtmlFromAssets() async {
+    String fileHtmlContents = await rootBundle.loadString(filePath);
+    _webViewController.loadUrl(Uri.dataFromString(fileHtmlContents,
+            mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
+        .toString());
+  }
+}
