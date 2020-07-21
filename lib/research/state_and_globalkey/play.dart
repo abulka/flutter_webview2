03f1c46d1cbb3f1ecd8e425dad2f32aff57ad15f
Andy Bulka <abulka@gmail.com>
2020-07-21 14:29:56 +1000

    finally got play globalkey example working

--- lib/research/state_and_globalkey/play.dart
+++ lib/research/state_and_globalkey/play.dart
@@ -20,7 +20,8 @@ class MyApp extends StatelessWidget {
             ),
             AndyStateless(),
             AndyStateful(),
-            Another(),
+            // Another(), // this never got access to the global 'another' key
+            Another(key: another),
             AnotherButton(),
           ],
         ),
@@ -82,23 +83,25 @@ class _AndyStatefulState extends State<AndyStateful> {
 
 // THIS IS HAIRY
 // Another StatefulWidget which exposes itself to modification using key
-// globalKey technique via 'another' doesn't work but my old 'another2' hack does
+// globalKey technique via 'another' - finally got it to work
+// Also my old 'another2' hack works too
 
 final another = GlobalKey<_AnotherState>();
 var another2;
 
 class Another extends StatefulWidget {
-  Another({Key key}) : super(key: key);
+  Another({Key key}) : super(key: key); // setting up to use pre-allocated key
+
   @override
   _AnotherState createState() {
-    another2 = _AnotherState();
+    another2 = _AnotherState(); // ANDY super hack, not using global key
     return another2;
   }
 }
 
 class _AnotherState extends State<Another> {
   var _msg = 'quantum';
-  final another3 = GlobalKey<_AnotherState>(); // perhaps if key is inside class
+  final another3 = GlobalKey<_AnotherState>(); // perhaps if key is inside class - NO DIFF
 
   set msg(value) {
     setState(() {
@@ -110,8 +113,12 @@ class _AnotherState extends State<Another> {
   Widget build(BuildContext context) {
     print('build $another $another2 $another3');
     return Padding(
+      // Actually don't use the global key here, pass it in from above
+      // otherwise .currentState of these keys is always null 😥
+      // Its probably because the 'Another' StatefulWidget itself needs to
+      // get the key and do it's super(key: key) call.
       // key: another,
-      key: another3,
+      // key: another3,
       padding: const EdgeInsets.all(8.0),
       child: Container(
         decoration: BoxDecoration(border: Border.all(color: Colors.blueAccent)),
@@ -126,6 +133,7 @@ class _AnotherState extends State<Another> {
               onPressed: () {
                 print('$another ${another.currentState} $another2  $another3 ${another3.currentState}');
                 // another3.currentState.msg = 'physics inside';  // official globalkey way does not, currentState is null
+                another.currentState.msg = 'physics inside';  // official globalkey way OK
               },
             )
           ],
@@ -145,8 +153,8 @@ class AnotherButton extends StatelessWidget {
         child: Text('am outside widget'),
         onPressed: () {
           print('$another ${another.currentState} $another2');
-          another2.msg = 'physics'; // my super hack works
-          // another.currentState.msg = 'physics';  // official globalkey way does not, currentState is null
+          // another2.msg = 'physics'; // my super hack works
+          another.currentState.msg = 'physics';  // official globalkey way works too! 🔆🔆
         },
       ),
     );
