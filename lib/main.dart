9fe23797c5aba623aebb8b5b39e25b21b40cdcb6
Andy Bulka <abulka@gmail.com>
2020-07-22 21:02:15 +1000

    evaluate math expressions works

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,8 @@ class _WebViewTestState extends State<WebViewTest> {
             _webViewController
                 // .evaluateJavascript('fred_add_via_timeout_which_posts(10, 10)');
                 // .evaluateJavascript('add(10, 10)');
-                .evaluateJavascript('mathjs1(10, 10)');
+                // .evaluateJavascript('mathjs1(10, 10)');
+                .evaluateJavascript('result = math.sqrt(-2).toString(); Toaster.postMessage(result)');
           },
         ),
       ),
