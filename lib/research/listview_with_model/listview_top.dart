77504eaf4293a71f9e01a4877f5ca67f1bb9af02
Andy Bulka <abulka@gmail.com>
2020-07-23 12:11:16 +1000

    added caclulations model

--- lib/research/listview_with_model/listview_top.dart
+++ lib/research/listview_with_model/listview_top.dart
@@ -3,6 +3,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:dart_random_choice/dart_random_choice.dart';
+import 'package:flutter_webview2/models/calculation.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -49,7 +51,8 @@ class BodyLayout extends StatelessWidget {
                   style:
                       TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold))),
         ),
-        Expanded(child: _myListView(context)),
+        // Expanded(child: _myListView(context)),
+        Expanded(child: _myListView2(context)),
         Container(
           height: 50,
           color: Colors.amber[200],
@@ -57,7 +60,9 @@ class BodyLayout extends StatelessWidget {
             children: <Widget>[
               RaisedButton(
                 child: Text('Add'),
-                onPressed: () => print('xx'),
+                onPressed: () {
+                  print('xx');
+                },
               )
             ],
           ),
@@ -70,6 +75,89 @@ class BodyLayout extends StatelessWidget {
     );
   }
 }
+
+// MODEL
+
+Widget _myListView2(BuildContext context) {
+  return AllCalculations();
+}
+
+/// It is generally considered a bad practice to enclose a huge widget tree
+/// inside a Consumer widget. This widget should be inserted as deep as possible
+/// in the widget tree to prevent unnecessary re-renders. For more info, see
+/// here. We need to re-render all the list items in case any of the task item
+/// changes. That's why I have enclosed the use of TaskList widget inside the
+/// Consumer. Now whenever our provider calls notifyListener in its model. It
+/// will re-render our TaskList widget.
+class AllCalculations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => CalculationsModel(),
+      child: Container(
+        child: Consumer<CalculationsModel>(
+          builder: (context, calculations, child) => CalculationList(
+            calculations: calculations.allCalculations,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CalculationList extends StatelessWidget {
+  final List<Calculation> calculations; // I guess this is the view model list
+
+  CalculationList({@required this.calculations});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: convertModelToWidgets(),
+    );
+  }
+
+  /// Converts the pure model list of calculations into a list of widgets
+  /// .map() returns a lazy iterable of the new thing, so .toList() forces computation
+  /// not sure why we can't leave it as iterable - surely the build() method can cope?
+  List<Widget> convertModelToWidgets() {
+    return calculations
+        .map((calculation) => CalculationListItem(calculation: calculation))
+        .toList();
+  }
+}
+
+class CalculationListItem extends StatelessWidget {
+  final Calculation calculation;
+
+  CalculationListItem({@required this.calculation});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Checkbox(
+        value: calculation.completed,
+        onChanged: (bool checked) {
+          Provider.of<CalculationsModel>(context, listen: false)
+              .toggleComplete(calculation);
+        },
+      ),
+      title: Text(calculation.expression),
+      trailing: IconButton(
+        icon: Icon(
+          Icons.delete,
+          color: Colors.red,
+        ),
+        onPressed: () {
+          Provider.of<CalculationsModel>(context, listen: false)
+              .delete(calculation);
+        },
+      ),
+    );
+  }
+}
+
+// ORI
 
 // replace this function with the code in the examples
 Widget _myListView(BuildContext context) {
