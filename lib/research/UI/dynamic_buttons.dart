5aca86895ec4cef9bd171af88cd0ff50719499e8
Andy Bulka <abulka@gmail.com>
2020-07-26 17:19:03 +1000

    dynamically create lots of buttons

--- lib/research/UI/dynamic_buttons.dart
+++ lib/research/UI/dynamic_buttons.dart
@@ -3,9 +3,19 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:dart_random_choice/dart_random_choice.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class MyModel extends ChangeNotifier {
+  List buttonLabels = ['Do A', 'Do B', 'Do C', 'Do D'];
+
+  void add(String label) {
+    buttonLabels.add(label);
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -13,14 +23,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ListView Experiments',
+      title: 'Dynamic Experiments',
       theme: ThemeData(
         primarySwatch: Colors.teal,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-
-      // home: MyHomePage(title: 'ListView experiments'),
-
       home: Scaffold(
         appBar: AppBar(title: Text('Creating things dynamically')),
         body: BodyLayout(),
@@ -32,17 +39,21 @@ class MyApp extends StatelessWidget {
 class BodyLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 50,
-          color: Colors.amber[500],
-          child: const Center(child: Text('... is what flutter does!')),
-        ),
-        inputArea(),
-        exampleButtonsArea(),
-      ],
-    );
+    return ChangeNotifierProvider(
+        create: (context) => MyModel(),
+        builder: (context, child) {
+          return Column(
+            children: [
+              Container(
+                height: 50,
+                color: Colors.amber[500],
+                child: const Center(child: Text('... is what flutter does!')),
+              ),
+              inputArea(),
+              exampleButtonsArea(),
+            ],
+          );
+        });
   }
 
   Expanded exampleButtonsArea() {
@@ -50,81 +61,83 @@ class BodyLayout extends StatelessWidget {
     List buttonLabels = ['Do a', 'Do b', 'Do c', 'Do d'];
     return Expanded(
       child: SingleChildScrollView(
-        child: Wrap(
-          children: [
-            RaisedButton(
-              onPressed: () {},
-              child: Text('example to inch'),
-            ),
-            RaisedButton(
-              onPressed: () {},
-              child: Text('example sin'),
-            ),
-            RaisedButton(
-              onPressed: () {},
-              child: Text('example math.pow'),
-            ),
-            RaisedButton(
-              onPressed: () {},
-              child: Text('complex({ r: sqrt(2), phi: pi / 4 })'),
-            ),
-            Container(
-              color: Colors.redAccent,
-              height: 30,
-              width: 130,
-            ),
-            Container(
-              color: Colors.red[200],
-              height: 30,
-              width: 130,
-            ),
-            Container(
-              color: Colors.red[300],
-              height: 30,
-              width: 130,
-            ),
-            Container(
-              color: Colors.red[400],
-              height: 30,
-              width: 130,
-            ),
-            Container(
-              color: Colors.red[600],
-              height: 30,
-              width: 130,
-            ),
-          ]
-            ..addAll(columnHeaders
-                .map((header) => new Container(
-                      color: randomChoice([
-                        Colors.amber,
-                        Colors.green,
-                        Colors.blue[400],
-                        Colors.orange,
-                        Colors.indigo[200]
-                      ]),
-                      alignment: FractionalOffset.center,
-                      width: 120.0,
-                      margin: EdgeInsets.all(3.0),
-                      padding: const EdgeInsets.only(
-                          top: 5.0, bottom: 5.0, right: 3.0, left: 3.0),
-                      child: new Text(
-                        header,
-                        style: TextStyle(color: Colors.grey[800]),
-                        textAlign: TextAlign.center,
-                      ),
-                    ))
-                .toList())
-            ..addAll(buttonLabels
-                .map((label) => Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RaisedButton(
-                        child: Text('example button $label'),
-                        onPressed: () {},
-                        padding: EdgeInsets.all(30),
-                      ),
-                ))
-                .toList()),
+        child: Consumer<MyModel>(
+          builder: (context, myModel, child) => Wrap(
+            children: [
+              RaisedButton(
+                child: Text('add a button'),
+                onPressed: () {
+                  Provider.of<MyModel>(context, listen: false).add('hi');
+                },
+              ),
+              RaisedButton(
+                child: Text('add lots of buttons'),
+                onPressed: () {
+                  Provider.of<MyModel>(context, listen: false)
+                    ..add('hey')
+                    ..add('there')
+                    ..add('how')
+                    ..add('are you?');
+                },
+              ),
+              Container(
+                color: Colors.redAccent,
+                height: 30,
+                width: 130,
+              ),
+              Container(
+                color: Colors.red[200],
+                height: 30,
+                width: 130,
+              ),
+              Container(
+                color: Colors.red[300],
+                height: 30,
+                width: 130,
+              ),
+              Container(
+                color: Colors.red[400],
+                height: 30,
+                width: 130,
+              ),
+              Container(
+                color: Colors.red[600],
+                height: 30,
+                width: 130,
+              ),
+            ]
+              ..addAll(columnHeaders
+                  .map((header) => new Container(
+                        color: randomChoice([
+                          Colors.amber,
+                          Colors.green,
+                          Colors.blue[400],
+                          Colors.orange,
+                          Colors.indigo[200]
+                        ]),
+                        alignment: FractionalOffset.center,
+                        width: 120.0,
+                        margin: EdgeInsets.all(3.0),
+                        padding: const EdgeInsets.only(
+                            top: 5.0, bottom: 5.0, right: 3.0, left: 3.0),
+                        child: new Text(
+                          'container $header',
+                          style: TextStyle(color: Colors.grey[800]),
+                          textAlign: TextAlign.center,
+                        ),
+                      ))
+                  .toList())
+              ..addAll(myModel.buttonLabels
+                  .map((label) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: RaisedButton(
+                          child: Text('$label'),
+                          onPressed: () {},
+                          padding: EdgeInsets.all(30),
+                        ),
+                      ))
+                  .toList()),
+          ),
         ),
       ),
     );
